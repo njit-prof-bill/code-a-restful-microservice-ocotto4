fd5a5f9ac30b8e9a6085762c83933ad53ec2b9df
Oscar Cotto <oc4@njit.edu>
2024-10-14 20:18:01 -0400

    Implemented API endpoints

--- src/index.js
+++ src/index.js
@@ -9,9 +9,43 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+let allUsers = [];
+app.get('/users/:id', (req, res) => {
+    const user = allUsers.find(i => i.id === parseInt(req.params.id));
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+    res.json(user);
 });
+
+app.post('/users', (req, res) => {
+    const newUser = {
+      id: allUsers.length + 1,
+      name: req.body.name,
+      email: req.body.email,
+    };
+    allUsers.push(newUser);
+    res.status(201).json(newUser);
+  });
+  
+  app.put('/users/:id', (req, res) => {
+    const user = allUsers.find(i => i.id === parseInt(req.params.id));
+    if (!user) {
+      return res.status(404).send('Item not found');
+    }
+    user.name = req.body.name;
+    user.email = req.body.email;
+    res.json(user);
+  });
+
+  app.delete('/users/:id', (req, res) => {
+    const userIndex = allUsers.findIndex(i => i.id === parseInt(req.params.id));
+    if (userIndex === -1) {
+      return res.status(404).send('User not found');
+    }
+    allUsers.splice(userIndex, 1);
+    res.status(204).send();
+  });
 
 // Do not touch the code below this comment
 // **************************************************************
